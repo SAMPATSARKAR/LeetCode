03d303b87ea7b0e0edc55a9c355177c7954a1c17
Sampat Sarkar <sampat7123@gmail.com>
2026-06-29 22:44:33 +0530

    Time: 0 ms (100%), Space: 18.1 MB (42.24%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,23 +1,8 @@
 class Solution {
 public:
-int pivot(vector<int> nums,int n){
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            int mid = i+(j-i)/2;
-                if(nums[mid]>=nums[0]){
-                    i=mid+1;
-                }else{
-                    j=mid;
-                }
-
-        }
-        return i;
-    }
-
-    bool binarySearch(vector<int> nums,int i1,int j1,int target){
-        int l=i1;
-        int r=j1;
+    bool search(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size()-1;
         int mid;
         while (l <= r) {
     int mid = l + (r - l) / 2;
@@ -40,17 +25,8 @@ int pivot(vector<int> nums,int n){
             l = mid + 1;
         else
             r = mid - 1;
+        }
     }
-}
-        return false;
-    }
-    bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int p = pivot(nums,n);
-        if(target>=nums[p] && target<=nums[n-1]){
-            return binarySearch(nums,p,n-1,target);
-        }else{
-            return binarySearch(nums,0,p-1,target);
-        } 
+    return false;
     }
 };
