4e3b78760d220142e6e55e9ee23767f121b5b26d
Sampat Sarkar <sampat7123@gmail.com>
2026-07-30 21:50:07 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,16 +2,8 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int res=0;
-        unordered_map<int,int> mp;
-        int assign_key=2;
-
         for(int i=0;i<word.size();i++){
-            if(assign_key>9){
-                assign_key=2;
-            }
-            mp[assign_key]++;
-            res+=mp[assign_key];
-            assign_key++;
+            res+=i/8+1;
         }
         return res;
     }
