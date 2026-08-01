dc790a8756fb952b005de9da198e1841f05ac04f
Sampat Sarkar <sampat7123@gmail.com>
2026-08-01 22:46:00 +0530

    Time: 0 ms (100%), Space: 11.8 MB (49.67%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -6,14 +6,16 @@ public:
             v[m[i]-'a']++;
         }
         for(auto x:r){
+            if(v[x-'a']==0) return false;
             v[x-'a']--;
 
         }
-        for(int i=0;i<26;i++){
-            if(v[i]<0){
-                return false;
-            }
-        }
+        // for(int i=0;i<26;i++){
+        //     i
+        //     if(v[i]<0){
+        //         return false;
+        //     }
+        // }
         return true;
     }
 };
