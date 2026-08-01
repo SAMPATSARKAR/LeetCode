89d08814377462d58370b8153ac48f6d23ffd4b8
Sampat Sarkar <sampat7123@gmail.com>
2026-08-01 09:58:00 +0530

    Time: 2 ms (77.9%), Space: 7.6 MB (98.94%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int fib(int n) {//tabulation
-        if(n==0) return 0;
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
+    int fib(int n) {//space optimization
+    if(n==1) return n;
+    int a=0;
+    int b =1;
+    int c=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-
+            c=a+b;
+            a=b;
+            b=c;
         }
-        return dp[n];
+        return c;
+
     }
 };
