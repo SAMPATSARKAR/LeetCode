94807040c02fe531b9876aad6563c410d05328c2
Sampat Sarkar <sampat7123@gmail.com>
2026-08-04 20:16:17 +0530

    Time: 1 ms (60.57%), Space: 33.7 MB (17.56%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
+        //brute force
+                    // int n=nums.size();
+                    // sort(nums.begin(),nums.end());
+                    // vector<int> v;
+                    // for(int i=0;i<n-1;i++){
+                    //     if(nums[i+1]-nums[i]!=1){
+                    //         int x = nums[i+1]-nums[i];
+                    //         int y = nums[i];
+                    //         for(int i=1;i<x;i++){
+                    //             v.push_back(y+i);
+                    //         }
+                    //     }
+                    // }
+                    // return v;
+        // optimize
         vector<int> v;
-        for(int i=0;i<n-1;i++){
-            if(nums[i+1]-nums[i]!=1){
-                int x = nums[i+1]-nums[i];
-                int y = nums[i];
-                for(int i=1;i<x;i++){
-                    v.push_back(y+i);
-                }
-            }
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        int s = nums[0];
+        int e = nums[n-1];
+        set<int> st(nums.begin(),nums.end());
+        for(int i=s+1;i<=e-1;i++){
+            if(st.count(i)==0) v.push_back(i);
         }
         return v;
 
