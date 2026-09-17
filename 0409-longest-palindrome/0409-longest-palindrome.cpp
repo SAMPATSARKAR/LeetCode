238fe5ecfa4af81079a79152a9c6eeb58026cca9
Sampat Sarkar <sampat7123@gmail.com>
2026-09-18 00:14:45 +0530

    Time: 0 ms (100%), Space: 9.4 MB (41.08%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int n=s.size();
-        unordered_map<char,int> mp;
-        for(int i=0;i<n;i++){
-            mp[s[i]]++;
+        unordered_map<char,int> mpp;
+        for(auto x:s){
+            mpp[x]++;
         }
         int sum=0;
         bool count=false;
-        for(auto x: mp){
+        for(auto x:mpp){
             if(x.second%2==0){
                 sum+=x.second;
-            }
-            else{
+            }else{
                 sum+=x.second-1;
                 count=true;
             }
         }
         if(count){
-            return sum+1;
+            sum+=1;
         }
         return sum;
     }
