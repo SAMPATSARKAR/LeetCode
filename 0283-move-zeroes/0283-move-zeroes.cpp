ffc486482bc67506c738f2c4ea0b7eaac75f6962
Sampat Sarkar <sampat7123@gmail.com>
2026-09-24 22:32:01 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.5%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int i=0;
         int j=0,n=nums.size();
+        // while(i<nums.size()){
+        //     if(nums[i]!=0 ){
+        //         swap(nums[i],nums[j]);
+        //         j++;
+        //     }
+        //     i++;
+
+        // }
+        int ind=0;
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                swap(nums[i],nums[j++]);
+                swap(nums[i],nums[ind++]);
             }
         }
-
-
     }
 };
